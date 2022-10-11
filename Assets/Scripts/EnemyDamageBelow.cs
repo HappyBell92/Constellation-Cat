84553f142251fba62f51f3c75f92105f4bd67a3c
HappyBell92 <kimi.kaleva.r@gmail.com>
2022-10-11 13:36:15 +0300

    Added extra jump on enemy head jump

--- Assets/Scripts/EnemyDamageBelow.cs
+++ Assets/Scripts/EnemyDamageBelow.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHurtBox : MonoBehaviour
+public class EnemyDamageBelow : MonoBehaviour
 {
     public float knockBackStrength = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Rigidbody playerRigidbody = other.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromEnemy = other.gameObject.transform.position - transform.position;
 
-            playerRigidbody.velocity = playerRigidbody.transform.up * knockBackStrength;
+            playerRigidbody.AddForce(awayFromEnemy * knockBackStrength, ForceMode.Impulse);
         }
     }
 }
