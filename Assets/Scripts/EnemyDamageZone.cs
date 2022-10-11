a256dfe4ce6e8a5d117eecc636c8429623d548a6
HappyBell92 <kimi.kaleva.r@gmail.com>
2022-10-11 13:01:23 +0300

    Added upwards knockback to enemy

--- Assets/Scripts/EnemyDamageZone.cs
+++ Assets/Scripts/EnemyDamageZone.cs
@@ -6,6 +6,7 @@ public class EnemyDamageZone : MonoBehaviour
 {
 
     public float knockBackStrength = 10.0f; // how hard the player gets knocked back
+    public float knockBackUp = 2.0f; // how hard the player gets knocked upwards
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class EnemyDamageZone : MonoBehaviour
             Vector3 awayFromEnemy = other.gameObject.transform.position - transform.position;
 
             playerRigidbody.AddForce(awayFromEnemy * knockBackStrength, ForceMode.Impulse);
+            playerRigidbody.AddForce(transform.up * knockBackUp, ForceMode.Impulse);
         }
     }
 }
