5b39a44a19168459bb8324010ba4dc31165816e1
HappyBell92 <kimi.kaleva.r@gmail.com>
2022-10-25 13:13:50 +0300

    Made skybox material

--- Assets/Scripts/BoxBreak.cs
+++ Assets/Scripts/BoxBreak.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BoxBreak : MonoBehaviour
 {
+    [SerializeField] GameObject star;
+    [SerializeField] GameObject stamp;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +20,10 @@ public class BoxBreak : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("PlayerAttack"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("I have broken!");
+            GameObject newStamp = Instantiate(stamp, transform.position + transform.up, transform.rotation * Quaternion.Euler(0, 0, 0));
             Destroy(this.gameObject);
         }
     }
